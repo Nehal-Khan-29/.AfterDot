9336ecf52197bbdefed65e6d6e98a4fbfb3737ff
Nehal Khan <128951002+Nehal-Khan-29@users.noreply.github.com>
2023-03-26 11:55:08 +0530

    Update AfterDot Python code.py

--- AfterDot Python code.py
+++ AfterDot Python code.py
@@ -398,7 +398,7 @@ def aboutpage():
     Button(about,text='Contact Us',font=('Arial',20),command=help,height=1,width=16,bg='white',
     fg='gray6',activebackground='Skyblue',activeforeground='thistle1').place(x=750,y=500)
 
-    Label(about,text=('''This program is created on 16 january 2023 by Nehal Khan.'''),font=('Arial',15),bg='white').place(x=250,y=200)
+    Label(about,text=('''This program is created on 25 March 2023 by Nehal Khan, Sridharan, Athish Vishnu.'''),font=('Arial',15),bg='white').place(x=250,y=200)
 
 
     about.mainloop()
